467860c0c0a53c3a384d2be7bd8b1c89f6fba3c1
Joakim Carlsson <tensh@spotify.com>
2016-12-15 23:35:08 +0100

    Solution for day 15

---
+++ 15/b.cpp
@@ -0,0 +1,44 @@
+
+#include <algorithm>
+#include <array>
+#include <assert.h>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+
+using List = std::array<size_t, 7>;
+
+size_t find_drop_point(const List &position_counts, List &positions) {
+  for (int i = 1; ; ++i) {
+    bool found = true;
+    for (int j = 0; j < positions.size(); ++j) {
+      const auto pos = positions[j];
+      const auto pos_count = position_counts[j];
+
+      const auto future_pos = (pos + j) % pos_count;
+      found &= future_pos == 0;
+
+      positions[j] = (pos + 1) % pos_count;
+    }
+    if (found) {
+      // Time starts at 0 (reduces index by 1)
+      // It takes 1s for capsule to reach first disc (reduces index by 1)
+      return i - 2;
+    }
+  }
+  throw "not found.";
+}
+
+int main() {
+  const List position_counts = { 17, 3, 19, 13, 7, 5, 11 };
+  List positions = { 15, 2, 4, 2, 2, 0, 0 };
+
+  const auto drop_point = find_drop_point(position_counts, positions);
+  std::cout << "1. Press button" << std::endl;
+  std::cout << "2. Pray" << std::endl;
+  std::cout << "3. ???" << std::endl;
+  std::cout << "4. Success! (" << drop_point << ")" << std::endl;
+
+  return 0;
+}
